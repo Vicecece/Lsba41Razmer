6607953d8a3f8f792ec25d2aaac12678e24222f4
Azamat Gilmanshin <ttapent@gmail.com>
2024-02-13 18:38:35 +0500

    лаба 18

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -20,15 +20,87 @@ namespace Gilmanshin41Razmer
     /// </summary>
     public partial class ProductPage : Page
     {
+        int countFull = 0;
         public ProductPage()
         {
+            
             InitializeComponent();
-            ProductListView.ItemsSource = Gilmanshin411Entities.GetContext().Product.ToList();
+            var currentProduct = Gilmanshin411Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProduct;
+            ComboType.SelectedIndex = 0;
+            countFull = Gilmanshin411Entities.GetContext().Product.Count();
+            UpdateProduct();
+        }
+
+        private void UpdateProduct()
+        {
+            var currentProduct = Gilmanshin411Entities.GetContext().Product.ToList();
+            
+            var count = Gilmanshin411Entities.GetContext().Product.Count();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 9.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount > 10 && p.ProductDiscountAmount <= 14.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount > 15 && p.ProductDiscountAmount <= 100)).ToList();
+            }
+
+            currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TboxSearch.Text.ToLower())).ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderBy(p => p.ProductCost).ToList();
+            }
+
+            count = currentProduct.Count;
+            CountTextBlock.Text = $"Выведено {count} из {countFull}";
+
+            ProductListView.ItemsSource = currentProduct;
+
         }
 
         private void GO_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TboxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
         }
     }
 }
